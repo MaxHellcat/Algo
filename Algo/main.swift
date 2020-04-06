3f73dd6563c9bd0371ecac3cb78cec0ffbf0d371
Max Reshetey <reshetey@chu.do>
2020-04-06 13:45:38 +0300

    Initial Commit

---
+++ Algo/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Algo
+//
+//  Created by Max Reshetey on 06.04.2020.
+//  Copyright © 2020 Magic Unicorn Inc. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
