522ac0e59d7fb7d800943b282713bf24a70ee55e
Max Reshetey <reshetey@chu.do>
2020-04-21 15:41:33 +0300

    Enhance SinglyLinkedList

--- Algo/SinglyLinkedList.swift
+++ Algo/SinglyLinkedList.swift
@@ -24,29 +24,48 @@ class SinglyLinkedList {
         }
     }
 
-    private var head: Node?
+    deinit {
+        print("Deinit \(self)")
+    }
 
-    func insert(node: Node) {
+    private(set) var head: Node?
 
-        node.next = head
-        head = node
+    // Push back if atNode is nil
+    func insert(node: Node, atNode: Node?) {
+
+        precondition(true, "The atNode must be in the list")
+
+        if atNode === head { // Nil head is also caught here
+
+            node.next = head
+            head = node
+        }
+        else if let prevNode = searchPrev(node: atNode) {
+
+            node.next = prevNode.next
+            prevNode.next = node
+        }
+        else {
+
+            preconditionFailure("The atNode must be in the list")
+        }
     }
 
     func remove(node: Node) {
 
+        precondition(true, "The node must be in the list")
+
         if node === head {
-            head = head?.next
-            return
+
+            head = node.next
         }
+        else if let prevNode = searchPrev(node: node) {
 
-        var prevNode = head
-
-        while prevNode != nil && prevNode?.next !== node {
-            prevNode = prevNode?.next
+            prevNode.next = node.next
         }
+        else {
 
-        if prevNode != nil {
-            prevNode?.next = node.next
+            preconditionFailure("The node must be in the list")
         }
     }
 
@@ -54,11 +73,50 @@ class SinglyLinkedList {
 
         var node = head
 
-        while node != nil && node?.key != key {
-            node = node?.next
+        while node != nil && node!.key != key {
+            node = node!.next
         }
 
         return node
+    }
+
+    // Returns tail if passed node is nil
+    private func searchPrev(node: Node?) -> Node? {
+
+        var prevNode = head
+
+        while prevNode != nil && prevNode!.next !== node {
+            prevNode = prevNode!.next
+        }
+
+        return prevNode
+    }
+}
+
+extension SinglyLinkedList {
+
+    class func test() {
+
+        let list = SinglyLinkedList()
+
+        for i in 1...10 {
+            list.insert(node: SinglyLinkedList.Node(key: i), atNode: nil)
+        }
+        print("List: \(list)")
+
+        let key = 7
+        print("Search node \(key)")
+        weak var node = list.search(key: key)
+        print("Found node: \(String(describing: node))")
+
+        let newNode = SinglyLinkedList.Node(key: 25)
+        print("Insert node \(newNode) at \(String(describing: node))")
+        list.insert(node: newNode, atNode: node)
+        print("List: \(list)")
+
+        print("Remove node \(newNode)")
+        list.remove(node: newNode)
+        print("List: \(list)")
     }
 }
 
@@ -85,7 +143,6 @@ extension SinglyLinkedList: CustomStringConvertible {
 extension SinglyLinkedList.Node: CustomStringConvertible {
 
     var description: String {
-
         return "(key: \(key))"
     }
 }
--- Algo/main.swift
+++ Algo/main.swift
@@ -23,7 +23,7 @@ var arr = Array<Int>(1...10)
 //arr.reverse()
 arr.shuffle()
 
-print("\(arr), sorted: \(isSorted(arr: arr))")
+//print("\(arr), sorted: \(isSorted(arr: arr))")
 
 let before = Date().timeIntervalSince1970
 
@@ -38,22 +38,7 @@ let before = Date().timeIntervalSince1970
 //let orderStatisticIter = randomizedSelectIterative(&arr, 0, arr.count-1, i)
 //print("The \(i)th order statistic: \(orderStatisticIter)")
 
-let list = SinglyLinkedList()
-
-list.insert(node: SinglyLinkedList.Node(key: 1))
-list.insert(node: SinglyLinkedList.Node(key: 2))
-list.insert(node: SinglyLinkedList.Node(key: 3))
-print("List: \(list)")
-
-weak var node = list.search(key: 2)
-if let node = node {
-    print("Found node: \(node)")
-}
-
-if let node = node {
-    list.remove(node: node)
-    print("List: \(list)")
-}
+SinglyLinkedList.test()
 
 let after = Date().timeIntervalSince1970
 var duration = after - before
