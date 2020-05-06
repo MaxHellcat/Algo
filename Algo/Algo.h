7079012e64fe5c6476361202d8607a91213b2ce4
Max Reshetey <reshetey@chu.do>
2020-05-06 20:34:13 +0300

    Switch from executable to framework

---
+++ Algo/Algo.h
@@ -0,0 +1,19 @@
+//
+//  Algo.h
+//  Algo
+//
+//  Created by Max Reshetey on 06.05.2020.
+//  Copyright © 2020 Magic Unicorn Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Algo.
+FOUNDATION_EXPORT double AlgoVersionNumber;
+
+//! Project version string for Algo.
+FOUNDATION_EXPORT const unsigned char AlgoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Algo/PublicHeader.h>
+
+
